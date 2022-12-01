940088b339a544a61d8b0175b2e6683b42ce8a99
riya kumari <kajalajay2001@gmail.com>
2022-12-01 20:42:16 +0530

    code check

---
+++ pallindrome.js
@@ -0,0 +1,10 @@
+ let str="Madam"
+ let bag=""
+ for(let i=str.length-1;i>=0;i++){
+   bag=bag+str[i]
+}
+ if(bag=str)
+ }
+   console.log("true")
+
+ 
---
+++ prime.js
@@ -0,0 +1,8 @@
+let i=0;
+let limits=50
+  while(i<=50){
+ if(i%2!==0){
+console.log(i);
+}
+i++;
+}
